aaecae79c60beba01b173ee5f6d1d3ed204607b2
mohamadjavad abolhasanifar <m.abolhasanifar@UGYM.LOCAL>
2024-11-05 11:12:46 +0330

    C

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import { Col, Container, Row, Stack } from "react-bootstrap";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className=" bg-dark">
+    <footer className=" bg-dark pt-4">
       <Container fluid="lg">
-        <Row xl={1} sm={1} md={2}>
+        <Row xs={1} sm={2} md={3} className=" row-gap-2 mb-2">
           <Col>
             <Stack>
               <a className="link-info  " href="#">
@@ -20,9 +21,56 @@ export default function Footer() {
               </a>
             </Stack>
           </Col>
+
+          <Col>
+            <Stack>
+              <a className="link-info fw-bold " href="#">
+                Link1
+              </a>
+
+              <a className="link-info fw-bold " href="#">
+                Link2
+              </a>
+
+              <a className="link-info fw-bold" href="#">
+                Link3
+              </a>
+            </Stack>
+          </Col>
+
+          <Col>
+            <p className="my-0 mb-2 text-light">Our Apps</p>
+            <Stack direction="horizontal" gap={1}>
+              <a href="#">
+                <img
+                  loading="lazy"
+                  src="images/app-store-logo.png"
+                  height={40}
+                  width={125}
+                  alt=""
+                />
+              </a>
+
+              <a href="#">
+                <img
+                  loading="lazy"
+                  src="images/google-play-logo.png"
+                  width={125}
+                  height={40}
+                  alt=""
+                />
+              </a>
+            </Stack>
+          </Col>
         </Row>
 
-        <p>CopyRight Receved</p>
+        <p
+          className={
+            "text-center m-0 text-opacity-50 text-light " + styles.textRights
+          }
+        >
+          All Rights Reserved
+        </p>
       </Container>
     </footer>
   );
